147d1f160d7702de63c2773d39bb1bfa1cbe9051
michaelservidio <michael@michaelservidio.com>
2022-12-23 22:23:53 -0500

    Added debugger/summary functionality

--- ghost_inputs.py
+++ ghost_inputs.py
@@ -1,124 +1,227 @@
 import gremlin
 import threading
 import math
+import time
 from gremlin.user_plugin import *
 from gremlin.util import log
+from collections import defaultdict
 
-#Plugin UI Configuration
-ui_mode = ModeVariable("Apply Filtering to","The mode to apply this filtering to")
-ui_is_debug = BoolVariable("Verbose Logging","Log every legitimate button press (instead of just Ghost Inputs)", False)
-ui_device_name = StringVariable("Physical Device Name","What to call this device in the log?","Stick")
-ui_physical_device = StringVariable("Physical Device GUID","Copy and paste from Tools > Device Information")
-ui_virtual_device = IntegerVariable("Virtual Device ID","Specify the vJoy device to map the stick to (based on the order of their indexing in Tools > Device Information",1,1,16)
-ui_button_filtering = BoolVariable("Button Filtering Enabled?","Actively filter ghost input?", True)
-ui_button_threshold = IntegerVariable("Button Limit Threshold","How many *buttons* pressed at once (within the Monitoring Timespan) constitute a Ghost Input (on a single device)? Default: 2",2,0,100)
-ui_button_timespan = IntegerVariable("Button Monitoring Timespan","How many ticks (16.66ms) to wait after a button press before checking for ghost input? Default: 5",5,1,20)
+# Plugin UI Configuration
+ui_mode = ModeVariable("Apply Filtering to", "The mode to apply this filtering to")
+ui_is_verbose = BoolVariable("Verbose Logging", "Log every legitimate button press (instead of just Ghost Inputs)",
+                             False)
+ui_device_name = StringVariable("Physical Device Name", "What to call this device in the log?", "Stick")
+ui_physical_device = StringVariable("Physical Device GUID", "Copy and paste from Tools > Device Information")
+ui_virtual_device = IntegerVariable("Virtual Device ID",
+                                    "Specify the vJoy device to map the stick to (based on the order of their indexing in Tools > Device Information",
+                                    1, 1, 16)
+ui_button_filtering = BoolVariable("Button Filtering Enabled?", "Actively filter ghost input?", True)
+ui_button_threshold = IntegerVariable("Button Limit Threshold",
+                                      "How many *buttons* pressed at once (within the Monitoring Timespan) constitute a Ghost Input (on a single device)? Default: 2",
+                                      2, 0, 100)
+ui_button_timespan = IntegerVariable("Button Monitoring Timespan",
+                                     "How many ticks (16.66ms) to wait after a button press before checking for ghost input? Default: 5",
+                                     5, 1, 20)
 
-#class for each physical joystick device, for filtering and mapping
-class FilteredDevice :
+
+class Debugger:
+    def __init__(self, is_verbose):
+        self.is_verbose = is_verbose
+        self.summary = {
+            'period': {
+                'ghost': 10,
+                'legitimate': 100
+            },
+            'recent': False,
+            'percentage': 0.0,
+            'start_time': time.localtime(),
+            'elapsed_time': 0.0,
+            'rate': 0.0
+        }
+        self.counts = {
+            'total': 0,
+            'total_blocked': 0,
+            'total_allowed': 0,
+            'by_button': defaultdict(int),
+            'by_simultaneity': defaultdict(int)
+        }
+
+    def update(self):
+        self.counts['total'] = self.counts['total_blocked'] + self.counts['total_allowed']
+        self.summary['percentage'] = (self.counts['total_blocked'] / self.counts['total']) * 100
+        self.summary['elapsed_time'] = time.mktime(time.localtime()) - time.mktime(self.summary['start_time'])
+        self.summary['per_minute'] = (self.counts['total_blocked'] / self.summary['elapsed_time']) * 60
+        self.summary['per_hour'] = self.summary['per_minute'] * 60
+
+    def log(self, msg, **args):
+        if msg is "ready":
+            # output general setup info
+            log("Ghost Input filtering on Profile [" + args['device'].mode + "]")
+            log("  for Physical Device \"" + args['device'].name + "\" [" + str(args['device'].device_guid) + "]")
+            log("  mapping to Virtual Device " + str(args['device'].vjoy_id))
+            if args['device'].button_filtering:
+                log("    ... Button Filtering enabled")
+            if self.is_verbose:
+                log("    ... Verbose logging enabled")
+        elif msg is "ghost":
+            # increment counters
+            self.counts['total_blocked'] += 1
+            self.counts['by_button'][args['event'].identifier] += 1
+            self.counts['by_simultaneity'][args['device'].concurrent_presses] += 1
+            # log ghost input
+            log("> GHOST INPUT blocked! [" + args['device'].mode + "] " + args['device'].name + " button " + str(
+                args['event'].identifier) + " was pressed (" + str(
+                args['device'].concurrent_presses) + " buttons at once)")
+        elif msg is "legitimate":
+            # increment counters
+            self.counts['total_allowed'] += 1
+            if self.is_verbose:
+                log("USER pressed: " + args['device'].name + " button " + str(args['event'].identifier))
+        elif msg is "summary":
+            # output a summary
+            log(" ")
+            log("[Summary for (" + args['device'].mode + ") \"" + args['device'].name + "\"]")
+            log("   Total Inputs Allowed          :  " + str(self.counts['total_allowed']))
+            log("   Total Ghost Inputs Blocked :  " + str(self.counts['total_blocked']))
+            log(" ")
+            log("   Elapsed Time        :  " + str(self.summary['elapsed_time']) + " seconds")
+            log("   Ghost Input %      :  " + str(self.summary['percentage']))
+            log("   Ghost Inputs/min   :  " + str(self.summary['per_minute']))
+            log("   Ghost Inputs/hr     :  " + str(self.summary['per_hour']))
+            log(" ")
+            log("   By Button")
+            for btn, cnt in self.counts['by_button'].items():
+                log("         (" + str(btn) + ")              :  " + str(cnt))
+            log("   By Simultaneity")
+            for simul, cnt in self.counts['by_simultaneity'].items():
+                log("         (" + str(simul) + ") at once     :  " + str(cnt))
+            log(" ")
+            self.recent_summary = False
+        else:
+            # output the message
+            log(msg)
+
+    def summarize(self, device):
+        if not self.summary['recent']:
+            self.summary['recent'] = True
+            self.update()
+            self.log("summary", device=device)
+
+
+# class for each physical joystick device, for filtering and mapping
+class FilteredDevice:
     def __init__(self,
-                    #device
-                    name, mode, device_guid, vjoy_id,
-                    #buttons
-                    button_filtering, button_timespan, button_threshold,
-                    #variables
-                    is_debug,
-                    tick_len = .01666, max_buttons = 36#, max_axes = 8
-                ):
-        
+                 # device
+                 name, mode, device_guid, vjoy_id,
+                 # buttons
+                 button_filtering, button_timespan, button_threshold,
+                 # variables
+                 tick_len=.01666, max_buttons=36  # , max_axes = 8
+                 ):
+
         self.name = name
         self.mode = mode
-        self.is_debug = is_debug
         self.device_guid = device_guid
         self.vjoy_id = vjoy_id
-        self.button_filtering = button_filtering
-        self.button_timespan = [math.ceil(float(button_timespan)/2),math.floor(float(button_timespan)/2)] if button_filtering else [0,0]
-        self.button_threshold = button_threshold
         self.tick_len = tick_len
+
+        self.button_filtering = button_filtering
+        self.button_timespan = [math.ceil(float(button_timespan) / 2) * self.tick_len,
+                                math.floor(float(button_timespan) / 2) * self.tick_len] if button_filtering else [0, 0]
+        self.button_threshold = button_threshold
         self.max_buttons = max_buttons
-        
+
         self.concurrent_presses = 0
-        
-        #create the decorator
-        self.decorator = gremlin.input_devices.JoystickDecorator(self.name, self.device_guid, self.mode)
-        
-        #for each button on the device
-        for i in range(1,self.max_buttons + 1):
-            #add a decorator function for when that button is pressed
+        self.block_count = 0
+
+        # create the decorator
+        self.decorator = gremlin.input_devices.JoystickDecorator(self.name, str(self.device_guid), self.mode)
+
+        # for each button on the device
+        for i in range(1, self.max_buttons + 1):
+            # add a decorator function for when that button is pressed
             @self.decorator.button(i)
-            #pass that info to the function that will check other button presses
-            def callback(event, vjoy, joy):        
-                #increment total buttons counter for this device (if this is a press)
-                if event.is_pressed:    
-                    self.increment()
-                           
-                #wait the first half of the delay timespan (set number of ticks), then check for ghost inputs
-                self.defer(self.button_timespan[0], self.filter_the_button, [event,vjoy,joy])
-                      
-        #Log that device is ready
-        log("Ghost Input filtering on Profile [" + self.mode + "]")
-        log("  for Physical Device \"" + self.name + "\" [" + self.device_guid + "]")
-        log("  mapping to Virtual Device " + str(self.vjoy_id))
-        if self.button_filtering:
-            log("    ... Button Filtering enabled")
-        if self.is_debug:
-            log("    ... Verbose logging enabled")
-    
-    def increment(self):
+            # pass that info to the function that will check other button presses
+            def callback(event, vjoy, joy):
+                # increment total buttons counter for this device (if this is a press)
+                if event.is_pressed:
+                    self.start_monitoring()
+
+                # wait the first half of the delay timespan (set number of ticks), then check for ghost inputs
+                defer(self.button_timespan[0], self.filter_the_button, [event, vjoy, joy])
+
+        # Log that device is ready
+        debugger.log("ready", device=self)
+
+    def start_monitoring(self):
         self.concurrent_presses += 1
-        
-    def decrement(self):
+
+    def end_monitoring(self):
         self.concurrent_presses -= 1
-        
-    def defer(self, ticks, func, params = []):
-        timer = threading.Timer(ticks * self.tick_len, func, params)
-        timer.start()
-    
-    #checks total number of buttons pressed, every time a new button is pressed within the configured timespan
-    #and maps the physical device to the virtual device if NOT a ghost input
-    def filter_the_button(self,event,vjoy,joy):   
-                
-        #if <threshold> or more buttons (including this callback's triggered button) are pressed, this is likely a ghost input
+
+    # checks total number of buttons pressed, every time a new button is pressed within the configured timespan
+    # and maps the physical device to the virtual device if NOT a ghost input
+    def filter_the_button(self, event, vjoy, joy):
+
+        # if <threshold> or more buttons (including this callback's triggered button) are pressed, this is likely a ghost input
         is_ghost = self.concurrent_presses >= self.button_threshold
-         
-        #if this is a ghost input on key press (not release)
+
+        global debugger
+
+        # if this is a ghost input on key press (not release)
         if event.is_pressed and is_ghost and self.button_filtering:
-            #log it
-            log("> GHOST INPUT blocked! " + self.name + " button " + str(event.identifier) + " was pressed (" + str(self.concurrent_presses) + " buttons at once)")
-        else:            
-            #otherwise, get the current state (after this much delay)
+            # log it
+            debugger.log("ghost", event=event, device=self)
+
+            # log a summary every n logs
+            if debugger.counts['total_blocked'] % debugger.summary['period']['ghost'] is 0:
+                defer(5, debugger.summarize, [self])
+        else:
+            # otherwise, get the current state (after this much delay)
             still_pressed = joy[event.device_guid].button(event.identifier).is_pressed
-            #and update the virtual joystick
+            # and update the virtual joystick
             vjoy[self.vjoy_id].button(event.identifier).is_pressed = still_pressed
-            
-            #log legitimate press, if debugging
-            if self.is_debug and still_pressed:
-                log("USER pressed: " + self.name + " button " + str(event.identifier))
-                
-        #after half the delay and evaluation, delay the next half, then decrement the pressed counter (if this was a press and not a release)
-        #enough time will have passed that this callback's button should no longer be used to determine a Ghost Input
+
+            # log legitimate press
+            if still_pressed:
+                debugger.log("legitimate", event=event, device=self)
+
+                # log a summary every n logs
+                if debugger.counts['total_allowed'] % debugger.summary['period']['legitimate'] is 0:
+                    defer(5, debugger.summarize, [self])
+
+        # after half the delay and evaluation, delay the next half, then decrement the pressed counter (if this was a press and not a release)
+        # enough time will have passed that this callback's button should no longer be used to determine a Ghost Input
         if event.is_pressed:
-            self.defer(self.button_timespan[1], self.decrement)
-            
-#grab user configuration
+            defer(self.button_timespan[1], self.end_monitoring)
+
+
+# helper functions
+def defer(time, func, args=[]):
+    timer = threading.Timer(time, func, args)
+    timer.start()
+
+
+# grab user configuration
 name = ui_device_name.value
 mode = ui_mode.value
 guid = ui_physical_device.value
 vjoy_id = ui_virtual_device.value
-is_debug = bool(ui_is_debug.value) #joystick gremlin has an issue with BoolVariable persistance(?)
-button_filtering = bool(ui_button_filtering.value) #joystick gremlin has an issue with BoolVariable persistance(?)
+is_verbose = bool(ui_is_verbose.value)  # joystick gremlin has an issue with BoolVariable persistance(?)
+button_filtering = bool(ui_button_filtering.value)  # joystick gremlin has an issue with BoolVariable persistance(?)
 button_timespan = ui_button_timespan.value
 button_threshold = ui_button_threshold.value
 
-#Initialize filtered device (which creates decorators to listen for and filter input)
 if guid:
+    # Initialize debugging logging
+    debugger = Debugger(is_verbose)
+    # Initialize filtered device (which creates decorators to listen for and filter input)
     filtered_device = FilteredDevice(
-        name, 
-        mode, 
-        guid, 
-        vjoy_id, 
-        button_filtering, 
-        button_timespan, 
-        button_threshold, 
-        is_debug
+        name,
+        mode,
+        guid,
+        vjoy_id,
+        button_filtering,
+        button_timespan,
+        button_threshold
     )
